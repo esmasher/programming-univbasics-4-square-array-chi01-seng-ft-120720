cbe4bddbfdf3f59cf861c279ee9ccef0b9786883
Esma Sherwani <esmasherr@gmail.com>
2020-12-01 19:23:24 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -19,13 +19,33 @@
  # counter +=1
   #end
 
-square_array = [1, 2, 3 ]
-results_array = []
-count = 0
+#square_array = [1, 2, 3 ]
+#results_array = []
+#count = 0
  
-while count < square_array.length do
-  results_array << square_array[count] * square_array[count]
-  count += 1
+#while count < square_array.length do
+ # results_array << square_array[count] * square_array[count]
+  
+  #count += 1
+#end
+ 
+#results_array
+
+
+
+
+
+
+def square_array(array)
+  new_array = []
+  index = 0
+ 
+  while index < array.length do
+    new_array << array[index] * array[index]
+    index += 1
+  end
+ 
+  new_array
 end
  
-results_array
+square_array([1,2,3,4])
